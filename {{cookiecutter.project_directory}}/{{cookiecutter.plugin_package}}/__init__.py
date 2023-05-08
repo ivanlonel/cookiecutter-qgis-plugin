e3bff6e5e42b84fa2cfe5abc933d427be5ed8e3c
Ivan Lonel <ivanlonel91@gmail.com>
2023-05-08 14:32:21 -0300

    Fix flake8-annotations ANN201
    Missing return type annotation for public function

--- {{cookiecutter.project_directory}}/{{cookiecutter.plugin_package}}/__init__.py
+++ {{cookiecutter.project_directory}}/{{cookiecutter.plugin_package}}/__init__.py
@@ -1,6 +1,5 @@
 import os
-
-from qgis.gui import QgisInterface
+from typing import TYPE_CHECKING
 {% if cookiecutter.use_qgis_plugin_tools %}
 from {{cookiecutter.plugin_package}}.qgis_plugin_tools.infrastructure.debugging import (  # noqa: F401
     setup_debugpy,
@@ -12,8 +11,13 @@ debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER", "").lower()
 if debugger in {"debugpy", "ptvsd", "pydevd"}:
     locals()["setup_" + debugger]()
 {% endif %}
+if TYPE_CHECKING:
+    from qgis.gui import QgisInterface
 
-def classFactory(iface: QgisInterface):  # noqa: N802
+    from {{cookiecutter.plugin_package}}.plugin import Plugin
+
+
+def classFactory(iface: "QgisInterface") -> "Plugin":  # noqa: N802
     from {{cookiecutter.plugin_package}}.plugin import Plugin
 
     return Plugin()
