4558f66c5115c739e79c0e70fce071c655410620
Ivan Lonel <ivanlonel91@gmail.com>
2023-05-08 13:59:35 -0300

    Remove duplicated `from`s in imports

--- {{cookiecutter.project_directory}}/{{cookiecutter.plugin_package}}/__init__.py
+++ {{cookiecutter.project_directory}}/{{cookiecutter.plugin_package}}/__init__.py
@@ -2,9 +2,11 @@ import os
 
 from qgis.gui import QgisInterface
 {% if cookiecutter.use_qgis_plugin_tools %}
-from {{cookiecutter.plugin_package}}.qgis_plugin_tools.infrastructure.debugging import setup_debugpy  # noqa F401
-from {{cookiecutter.plugin_package}}.qgis_plugin_tools.infrastructure.debugging import setup_ptvsd  # noqa F401
-from {{cookiecutter.plugin_package}}.qgis_plugin_tools.infrastructure.debugging import setup_pydevd  # noqa F401
+from {{cookiecutter.plugin_package}}.qgis_plugin_tools.infrastructure.debugging import (  # noqa F401
+    setup_debugpy,
+    setup_ptvsd,
+    setup_pydevd,
+)
 
 debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER", "").lower()
 if debugger in {"debugpy", "ptvsd", "pydevd"}:
