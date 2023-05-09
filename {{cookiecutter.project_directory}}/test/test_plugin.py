404975be3debbe31e8e753a9fd6ced09a7af77ce
Ivan Lonel <ivanlonel91@gmail.com>
2023-05-09 17:00:06 -0300

    Create simple test for when submodule is not used

--- {{cookiecutter.project_directory}}/test/test_plugin.py
+++ {{cookiecutter.project_directory}}/test/test_plugin.py
@@ -1,5 +1,13 @@
+{%- if cookiecutter.use_qgis_plugin_tools|lower != "n" -%}
 from {{cookiecutter.plugin_package}}.qgis_plugin_tools.tools.resources import plugin_name
 
 
 def test_plugin_name():
     assert plugin_name() == "{{cookiecutter.plugin_name|replace(' ', '')}}"
+{%- else -%}
+from {{cookiecutter.plugin_package}}.plugin import Plugin
+
+
+def test_plugin_name():
+    assert Plugin.name == "{{cookiecutter.plugin_name|replace(' ', '')}}"
+{%- endif %}
